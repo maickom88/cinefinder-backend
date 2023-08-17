f99c6dad3bec9257671f50abc3868189b6d5f81c
Michael Frank <maickomtutos@gmail.com>
2023-08-17 16:34:47 -0300

    🚨 test: make modify in tests for mock

--- src/tests/controllers/test_movie_controller.py
+++ src/tests/controllers/test_movie_controller.py
@@ -3,6 +3,7 @@ from typing import List
 
 import pydantic
 import pytest
+from starlette.responses import JSONResponse
 from starlette.testclient import TestClient
 
 from src.config.config_app import init_app
@@ -14,23 +15,21 @@ from src.services.movies_services.movie_service import MovieService
 client = TestClient(init_app())
 
 
-def mocked_response():
-    raise HTTPException(status_code=400, detail='gibberish')
-
-
 def test_for_root_application():
     response = client.get("/")
     assert response.status_code == 404
 
 
-def test_should_list_movies_for_name():
+def test_should_list_movies_for_name(monkeypatch: pytest.MonkeyPatch):
+    monkeypatch.setattr(MovieService, "get_movies_by_query", sucess_request)
     params = {"name": "rambo"}
     response = client.get("/movie", params=params)
     assert bool(response.json())
     assert response.status_code == 200
 
 
-def test_should_list_empty():
+def test_should_list_empty(monkeypatch: pytest.MonkeyPatch):
+    monkeypatch.setattr(MovieService, "get_movies_by_query", empty_list)
     params = {"name": ""}
     response = client.get("/movie", params=params)
     results = pydantic.parse_obj_as(List[Movie], response.json())
@@ -68,3 +67,38 @@ def replace_error_not_found(*_, **__):
 
 def replace_error_unexpected(*_, **__):
     raise ApiBaseException()
+
+
+def empty_list(*_, **__):
+    return JSONResponse(
+        status_code=200,
+        content=[]
+    )
+
+
+def sucess_request(*_, **__):
+    return JSONResponse(
+        status_code=200,
+        content=''' 
+         {
+      "adult": false,
+      "backdrop_path": "/7drO1kYgQ0PnnU87sAnBEphYrSM.jpg",
+      "genre_ids": [
+        16,
+        28,
+        27
+      ],
+      "id": 1083862,
+      "original_language": "ja",
+      "original_title": "バイオハザード：デスアイランド",
+      "overview": "In San Francisco, Jill Valentine is dealing with a zombie outbreak and a new T-Virus, Leon Kennedy is on the trail of a kidnapped DARPA scientist, and Claire Redfield is investigating a monstrous fish that is killing whales in the bay. Joined by Chris Redfield and Rebecca Chambers, they discover the trail of clues from their separate cases all converge on the same location, Alcatraz Island, where a new evil has taken residence and awaits their arrival.",
+      "popularity": 912.474,
+      "poster_path": "/xzAQ28moSPEZxOHJ7WL1mX6hb5H.jpg",
+      "release_date": "2023-06-22",
+      "title": "Resident Evil: Death Island",
+      "video": false,
+      "vote_average": 7.708,
+      "vote_count": 542
+    }
+        '''
+    )
